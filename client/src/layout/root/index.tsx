c46fc3aa9f0527629760ab8d80c454adb6a45d8a
kennedyng <68716247+kennedyng@users.noreply.github.com>
2023-04-10 15:46:51 +0200

    Update index.tsx

--- client/src/layout/root/index.tsx
+++ client/src/layout/root/index.tsx
@@ -1,7 +1,16 @@
+import { Button } from "@mui/material";
 import React from "react";
+import { Link } from "react-router-dom";
 
 const RootLayout = () => {
-  return <div>RootLayout</div>;
+  return (
+    <div>
+      RootLayout
+      <Button component={Link} to="/auth">
+        Login
+      </Button>
+    </div>
+  );
 };
 
 export default RootLayout;
