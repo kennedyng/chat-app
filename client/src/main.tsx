bdec630a2949a50d143865b52c696765b76c43b9
kennedyng <68716247+kennedyng@users.noreply.github.com>
2023-04-24 17:30:29 +0200

    added react query

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,19 +1,23 @@
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "react-query";
 import App from "./App";
 import DrawerProvider from "./context/drawer";
 import "./index.css";
 import { theme } from "./theme";
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <CssBaseline />
-
-      <DrawerProvider>
-        <App />
-      </DrawerProvider>
+      <QueryClientProvider client={queryClient}>
+        <DrawerProvider>
+          <App />
+        </DrawerProvider>
+      </QueryClientProvider>
     </ThemeProvider>
   </React.StrictMode>
 );
