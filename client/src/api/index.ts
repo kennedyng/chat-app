685960984697e6b4b171c160ce5c04521c258284
kennedyng <68716247+kennedyng@users.noreply.github.com>
2023-08-04 09:19:18 +0200

    new changes

--- client/src/api/index.ts
+++ client/src/api/index.ts
@@ -1,4 +1,6 @@
 //export const API_URL = "https://chat-app-baff.vercel.app";
 
-export const API_URL = "https://chat-app-production-ba2b.up.railway.app";
+// export const API_URL = "https://chat-app-production-ba2b.up.railway.app";
 // export const API_URL = "http://localhost:3000";
+
+export const API_URL = "https://chat-app-g.adaptable.app";
